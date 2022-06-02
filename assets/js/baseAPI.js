a0b07c0f9d0d79acc8fa16849232e2326aaffe29
zhangzongyi <1521940735@qq.com>
2022-06-02 15:49:56 +0800

    完成了文章模块

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,16 +5,16 @@ $.ajaxPrefilter((option) => {
     // console.log(option);
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
     option.url = `http://www.liulongbin.top:3007` + option.url;
-    /* if(option.url.includes('/my/')){
-        option.headers = {
-            Authorization:localStorage.getItem("token"),
-        };
-    } */
-    if(option.url.indexOf('/my/') !== -1){
+    if(option.url.includes("/my/")){
         option.headers = {
             Authorization:localStorage.getItem("token"),
         };
     }
+    /* if(option.url.indexOf('/my/') !== -1){
+        option.headers = {
+            Authorization:localStorage.getItem("token"),
+        };
+    } */
     // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
     option.complete = (res) => {
         if(res.responseJSON.status ===1 && res.responseJSON.message === "身份认证失败！") {
--- assets/js/index.js
+++ assets/js/index.js
@@ -47,5 +47,8 @@ $("#btnlogout").click(()=>{
     });
 })
 
+function change(){
+    $('#change').attr('class','layui-this').next().attr('class','');
+}
 
 
